67c35c968405cfd11fe3bd7eef9d10dc94bcb3e2
Victor Artwood <victorartwood@gmail.com>
2023-05-01 19:15:32 +0300

    factory faker updates

--- database/factories/PaymentFactory.php
+++ database/factories/PaymentFactory.php
@@ -17,7 +17,12 @@ class PaymentFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'customer_id' => rand(1, 10),
+            'service_id' => rand(1, 10),
+            'from' => $this->faker->dateTimeInInterval('-2 month', '-1 month'),
+            'to' => $this->faker->dateTimeInInterval('+1 month', '+2 month'),
+            'amount' => rand(100, 999),
+            'active' => rand(0, 2),
         ];
     }
 }
